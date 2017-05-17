61d89fd7b0efb2b2542ba7737aa1ee5bc0f90b1a
doug.nelson <dnelson@simplec.com>
2017-05-17 10:12:57 -0400

    .

--- src/android/progress-dialog.ts
+++ src/android/progress-dialog.ts
@@ -1,70 +1,52 @@
-"use strict";
-var application = require("application");
-var LoadingIndicator = (function () {
-    function LoadingIndicator() {
+import {OptionsCommon} from '../interfaces';
+import * as application from 'application';
+
+declare var android: any;
+
+export class LoadingIndicator {
+  private _progressDialog: any;
+
+  public show(options?: OptionsCommon) {
+    let context = this._getContext();
+    if (context) {
+      if (typeof options === 'undefined') options = {};
+      if (typeof this._progressDialog === 'undefined') {
+        // Create
+        let indeterminate = true;
+        let cancelable = false;
+        if (options.android) {
+          if (options.android.indeterminate !== undefined) indeterminate = options.android.indeterminate;
+          if (options.android.cancelable !== undefined) cancelable = options.android.cancelable;
+        }
+
+        this._progressDialog = android.app.ProgressDialog.show(context, "", options.message || "Loading", indeterminate, cancelable);
+      } else if (this._progressDialog) {
+        // options
+        if (options.message && this._progressDialog.setMesssage) this._progressDialog.setMesssage(options.message);
+        if (options.progress) this._progressDialog.setProgress(options.progress);
+        // android specific
+        if (options.android) {
+          if (options.android.indeterminate) this._progressDialog.setIndeterminate(true);
+          if (options.android.max) this._progressDialog.setMax(options.android.max);
+          if (options.android.progressNumberFormat) this._progressDialog.setProgressNumberFormat(options.android.progressNumberFormat);
+          if (options.android.progressPercentFormat) this._progressDialog.setProgressPercentFormat(options.android.progressPercentFormat);
+          if (options.android.progressStyle) this._progressDialog.setProgressStyle(options.android.progressStyle);
+          if (options.android.secondaryProgress) this._progressDialog.setSecondaryProgress(options.android.secondaryProgress);
+        }
+      }
+      return this._progressDialog;
     }
-    LoadingIndicator.prototype.show = function (options) {
-        var context = this._getContext();
-        if (context) {
-            if (typeof options === 'undefined')
-                options = {};
-            
-            var progressStyle = 0;
-            if (options.android && options.android.progressStyle) {
-                progressStyle = options.android.progressStyle;
-            }
-            if (typeof this._progressDialog === 'undefined' || this._progressStyle!=progressStyle) {
-                if (this._progressDialog) {
-                    this._progressDialog.hide();
-                    this._progressDialog.dismiss();
-                }
-                this._progressDialog = new android.app.ProgressDialog(context);
-            }
+  }
 
-            {
-                var cancelable = false;
-                if (options.android.cancelable !== undefined)
-                       cancelable = options.android.cancelable;
+  public hide() {
+    if (typeof this._progressDialog !== 'undefined') {
+      this._progressDialog.hide();
+      this._progressDialog.dismiss();
+    }
+    this._progressDialog = undefined;
+  }
 
-                // options
-                if (options.message) {
-                    this._progressDialog.setMessage(options.message);
-                }
-                if (options.progress)
-                    this._progressDialog.setProgress(options.progress);
-                // android specific
-                if (options.android) {
-                    this._progressDialog.setCancelable(cancelable);
-                    if (options.android.indeterminate)
-                        this._progressDialog.setIndeterminate(true);
-                    if (options.android.max)
-                        this._progressDialog.setMax(options.android.max);
-                    if (options.android.progressNumberFormat)
-                        this._progressDialog.setProgressNumberFormat(options.android.progressNumberFormat);
-                    if (options.android.progressPercentFormat)
-                        this._progressDialog.setProgressPercentFormat(options.android.progressPercentFormat);
-                    if (options.android.progressStyle) {
-                        this._progressStyle = options.android.progressStyle;
-                        this._progressDialog.setProgressStyle(options.android.progressStyle);
-                    }
-                    if (options.android.secondaryProgress)
-                        this._progressDialog.setSecondaryProgress(options.android.secondaryProgress);
-                }
-                this._progressDialog.show();
-            }
-            return this._progressDialog;
-        }
-    };
-    LoadingIndicator.prototype.hide = function () {
-        if (typeof this._progressDialog !== 'undefined') {
-            this._progressDialog.hide();
-            this._progressDialog.dismiss();
-        }
-        this._progressDialog = undefined;
-    };
-    LoadingIndicator.prototype._getContext = function () {
-        return application.android.foregroundActivity;
-    };
-    return LoadingIndicator;
-}());
-exports.LoadingIndicator = LoadingIndicator;
+  private _getContext() {
+    return application.android.foregroundActivity;
+  }
+}
